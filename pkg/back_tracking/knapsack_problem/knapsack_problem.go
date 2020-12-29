039d4c069402dd114010be6cf89ecb7e15a6cdd7
404200721@qq.com <404200721@qq.com>
2020-12-29 18:39:46 +0800

    none

--- pkg/back_tracking/knapsack_problem/knapsack_problem.go
+++ pkg/back_tracking/knapsack_problem/knapsack_problem.go
@@ -1,5 +1,25 @@
 package kp
 
-func kp() {
-	
+import (
+	"fmt"
+	"math"
+)
+
+var maxw = math.MinInt16
+
+// kp return the max weight
+// items := []int{20, 19, 18, 7, 8}
+func kp(items []int, w, cw, i int) {
+	fmt.Println("maxw=", maxw, "cw=", cw, "i=", i)
+	if cw == w || i == len(items) {
+		if cw > maxw {
+			maxw = cw
+		}
+		return
+	}
+
+	kp(items, w, cw, i+1)
+	if cw+items[i] <= w {
+		kp(items, w, cw+items[i], i+1)
+	}
 }
--- pkg/back_tracking/knapsack_problem/knapsack_problem_test.go
+++ pkg/back_tracking/knapsack_problem/knapsack_problem_test.go
@@ -1,7 +1,13 @@
 package kp
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
-func test(t *testing.T){
-	kp()	
+func Test_kp(t *testing.T) {
+	items := []int{20, 19, 18, 7, 8}
+	w := 30
+	kp(items, w, 0, 0)
+	fmt.Println(maxw)
 }
