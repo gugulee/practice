92a0cd7fae0e43422b619569bc431c4d844e0bda
liguo <li.guo@99cloud.net>
2023-06-08 06:22:36 +0800

    none

--- scratch/dynamic_client/dynamic_client.go
+++ scratch/dynamic_client/dynamic_client.go
@@ -1,143 +1,143 @@
 package dynamicclient
 
-import (
-	"context"
-	"fmt"
-	"strings"
+// import (
+// 	"context"
+// 	"fmt"
+// 	"strings"
 
-	appsv1 "k8s.io/api/apps/v1"
-	"k8s.io/apimachinery/pkg/api/meta"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
-	"k8s.io/apimachinery/pkg/runtime"
-	"k8s.io/apimachinery/pkg/runtime/schema"
-	"k8s.io/client-go/dynamic"
-	"k8s.io/client-go/kubernetes"
-	"k8s.io/client-go/tools/clientcmd"
-)
+// 	appsv1 "k8s.io/api/apps/v1"
+// 	"k8s.io/apimachinery/pkg/api/meta"
+// 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+// 	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
+// 	"k8s.io/apimachinery/pkg/runtime"
+// 	"k8s.io/apimachinery/pkg/runtime/schema"
+// 	"k8s.io/client-go/dynamic"
+// 	"k8s.io/client-go/kubernetes"
+// 	"k8s.io/client-go/tools/clientcmd"
+// )
 
-type foo struct {
-	dynamicClient *dynamic.DynamicClient
-	kubeClient    *kubernetes.Clientset
-}
+// type foo struct {
+// 	dynamicClient *dynamic.DynamicClient
+// 	kubeClient    *kubernetes.Clientset
+// }
 
-func New(configFile string) (*foo, error) {
-	// Create a new Kubernetes API client
-	config, err := clientcmd.BuildConfigFromFlags("", configFile)
-	if err != nil {
-		return nil, err
-	}
+// func New(configFile string) (*foo, error) {
+// 	// Create a new Kubernetes API client
+// 	config, err := clientcmd.BuildConfigFromFlags("", configFile)
+// 	if err != nil {
+// 		return nil, err
+// 	}
 
-	kubeClient, err := kubernetes.NewForConfig(config)
-	if err != nil {
-		return nil, err
-	}
+// 	kubeClient, err := kubernetes.NewForConfig(config)
+// 	if err != nil {
+// 		return nil, err
+// 	}
 
-	client, err := dynamic.NewForConfig(config)
-	if err != nil {
-		return nil, err
-	}
+// 	client, err := dynamic.NewForConfig(config)
+// 	if err != nil {
+// 		return nil, err
+// 	}
 
-	return &foo{
-		dynamicClient: client,
-		kubeClient:    kubeClient,
-	}, nil
-}
+// 	return &foo{
+// 		dynamicClient: client,
+// 		kubeClient:    kubeClient,
+// 	}, nil
+// }
 
-func (f *foo) List(gvr schema.GroupVersionResource, namespace string) (*unstructured.UnstructuredList, error) {
-	// Get the list of all pods in the cluster when namespace is ""
-	objs, err := f.dynamicClient.Resource(gvr).Namespace(namespace).List(context.TODO(), metav1.ListOptions{})
-	if err != nil {
-		return nil, fmt.Errorf("list %s in namespace %s: %w", gvr.String(), namespace, err)
-	}
+// func (f *foo) List(gvr schema.GroupVersionResource, namespace string) (*unstructured.UnstructuredList, error) {
+// 	// Get the list of all pods in the cluster when namespace is ""
+// 	objs, err := f.dynamicClient.Resource(gvr).Namespace(namespace).List(context.TODO(), metav1.ListOptions{})
+// 	if err != nil {
+// 		return nil, fmt.Errorf("list %s in namespace %s: %w", gvr.String(), namespace, err)
+// 	}
 
-	return objs, nil
-}
+// 	return objs, nil
+// }
 
-func (f *foo) Get(gvr schema.GroupVersionResource, namespace, name string) (*unstructured.Unstructured, error) {
-	obj, err := f.dynamicClient.Resource(gvr).Namespace(namespace).Get(context.TODO(), name, metav1.GetOptions{})
-	if err != nil {
-		return nil, fmt.Errorf("get %s %s/%s: %w", gvr.String(), namespace, name, err)
-	}
+// func (f *foo) Get(gvr schema.GroupVersionResource, namespace, name string) (*unstructured.Unstructured, error) {
+// 	obj, err := f.dynamicClient.Resource(gvr).Namespace(namespace).Get(context.TODO(), name, metav1.GetOptions{})
+// 	if err != nil {
+// 		return nil, fmt.Errorf("get %s %s/%s: %w", gvr.String(), namespace, name, err)
+// 	}
 
-	return obj, nil
-}
+// 	return obj, nil
+// }
 
-func (f *foo) GetDeployment(namespace, name string) (*appsv1.Deployment, error) {
-	gvr := schema.GroupVersionResource{Group: appsv1.SchemeGroupVersion.Group, Version: appsv1.SchemeGroupVersion.Version, Resource: "deployments"}
-	obj, err := f.Get(gvr, namespace, name)
-	if err != nil {
-		return nil, err
-	}
+// func (f *foo) GetDeployment(namespace, name string) (*appsv1.Deployment, error) {
+// 	gvr := schema.GroupVersionResource{Group: appsv1.SchemeGroupVersion.Group, Version: appsv1.SchemeGroupVersion.Version, Resource: "deployments"}
+// 	obj, err := f.Get(gvr, namespace, name)
+// 	if err != nil {
+// 		return nil, err
+// 	}
 
-	deploy := &appsv1.Deployment{}
-	err = runtime.DefaultUnstructuredConverter.FromUnstructured(obj.UnstructuredContent(), deploy)
-	if err != nil {
-		return nil, err
-	}
+// 	deploy := &appsv1.Deployment{}
+// 	err = runtime.DefaultUnstructuredConverter.FromUnstructured(obj.UnstructuredContent(), deploy)
+// 	if err != nil {
+// 		return nil, err
+// 	}
 
-	return deploy, nil
-}
+// 	return deploy, nil
+// }
 
-// Update update the given object,
-// obj is a k8s resource or a custom resource which implements the Object and Type interfaces
-func (f *foo) Update(obj interface{}) error {
-	objectMeta, ok := obj.(metav1.Object)
-	if !ok {
-		return fmt.Errorf("obj has no ObjectMeta")
-	}
+// // Update update the given object,
+// // obj is a k8s resource or a custom resource which implements the Object and Type interfaces
+// func (f *foo) Update(obj interface{}) error {
+// 	objectMeta, ok := obj.(metav1.Object)
+// 	if !ok {
+// 		return fmt.Errorf("obj has no ObjectMeta")
+// 	}
 
-	typeMeta, ok := obj.(schema.ObjectKind)
-	if !ok {
-		return fmt.Errorf("obj has no TypeMeta")
-	}
+// 	typeMeta, ok := obj.(schema.ObjectKind)
+// 	if !ok {
+// 		return fmt.Errorf("obj has no TypeMeta")
+// 	}
 
-	gvk := typeMeta.GroupVersionKind()
-	gvr := kindToResource(gvk)
+// 	gvk := typeMeta.GroupVersionKind()
+// 	gvr := kindToResource(gvk)
 
-	data, err := runtime.DefaultUnstructuredConverter.ToUnstructured(obj)
-	if err != nil {
-		return err
-	}
+// 	data, err := runtime.DefaultUnstructuredConverter.ToUnstructured(obj)
+// 	if err != nil {
+// 		return err
+// 	}
 
-	_, err = f.dynamicClient.Resource(gvr).Namespace(objectMeta.GetNamespace()).Update(context.TODO(), &unstructured.Unstructured{Object: data}, metav1.UpdateOptions{})
-	if err != nil {
-		return err
-	}
+// 	_, err = f.dynamicClient.Resource(gvr).Namespace(objectMeta.GetNamespace()).Update(context.TODO(), &unstructured.Unstructured{Object: data}, metav1.UpdateOptions{})
+// 	if err != nil {
+// 		return err
+// 	}
 
-	return nil
-}
+// 	return nil
+// }
 
-func (f *foo) Create(obj interface{}) error {
-	objectMeta, err := meta.Accessor(obj)
-	if err != nil {
-		return fmt.Errorf("obj has no ObjectMeta: %v", err)
-	}
+// func (f *foo) Create(obj interface{}) error {
+// 	objectMeta, err := meta.Accessor(obj)
+// 	if err != nil {
+// 		return fmt.Errorf("obj has no ObjectMeta: %v", err)
+// 	}
 
-	typeMeta, ok := obj.(schema.ObjectKind)
-	if !ok {
-		return fmt.Errorf("obj has no TypeMeta")
-	}
+// 	typeMeta, ok := obj.(schema.ObjectKind)
+// 	if !ok {
+// 		return fmt.Errorf("obj has no TypeMeta")
+// 	}
 
-	gvk := typeMeta.GroupVersionKind()
-	gvr := kindToResource(gvk)
+// 	gvk := typeMeta.GroupVersionKind()
+// 	gvr := kindToResource(gvk)
 
-	data, err := runtime.DefaultUnstructuredConverter.ToUnstructured(obj)
-	if err != nil {
-		return err
-	}
+// 	data, err := runtime.DefaultUnstructuredConverter.ToUnstructured(obj)
+// 	if err != nil {
+// 		return err
+// 	}
 
-	_, err = f.dynamicClient.Resource(gvr).Namespace(objectMeta.GetNamespace()).Create(context.TODO(), &unstructured.Unstructured{Object: data}, metav1.CreateOptions{})
-	if err != nil {
-		return err
-	}
+// 	_, err = f.dynamicClient.Resource(gvr).Namespace(objectMeta.GetNamespace()).Create(context.TODO(), &unstructured.Unstructured{Object: data}, metav1.CreateOptions{})
+// 	if err != nil {
+// 		return err
+// 	}
 
-	return nil
-}
+// 	return nil
+// }
 
-func kindToResource(gvk schema.GroupVersionKind) schema.GroupVersionResource {
-	gv := gvk.GroupVersion()
-	kind := gvk.Kind
-	resource := gv.WithResource(strings.ToLower(kind) + "s")
-	return resource
-}
+// func kindToResource(gvk schema.GroupVersionKind) schema.GroupVersionResource {
+// 	gv := gvk.GroupVersion()
+// 	kind := gvk.Kind
+// 	resource := gv.WithResource(strings.ToLower(kind) + "s")
+// 	return resource
+// }
---
+++ scratch/merge_map/merge_map_test.go
@@ -0,0 +1,47 @@
+package mergemap
+
+import (
+	"encoding/json"
+	"os"
+	"testing"
+)
+
+func Test_Merge(t *testing.T){
+	base, err := os.ReadFile("/tmp/base.json")
+	if err != nil {
+		panic(err)
+	}
+
+	target, err := os.ReadFile("/tmp/target.json")
+	if err != nil {
+		panic(err)
+	}
+
+	// Create maps to hold the JSON objects
+	var src map[string]interface{}
+	var dst map[string]interface{}
+
+	// Unmarshal JSON strings into maps
+	if err := json.Unmarshal([]byte(base), &src); err != nil {
+		panic(err)
+	}
+
+	if err := json.Unmarshal([]byte(target), &dst); err != nil {
+		panic(err)
+	}
+
+	// Merge the two maps
+	merged := Merge(src, dst)
+	// merged := mergeMaps(obj1, obj2)
+
+	// Convert the merged map back to JSON
+	mergedJSON, err := json.Marshal(merged)
+	if err != nil {
+		panic(err)
+	}
+
+	err = os.WriteFile("/tmp/merge.json", mergedJSON, 0644)
+	if err != nil {
+		panic(err)
+	}
+}
