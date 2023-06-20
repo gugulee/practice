3550911c789d81a90a51796fc4acbdbb9657367f
liguo <li.guo@99cloud.net>
2023-06-20 18:23:19 +0800

    none

--- main.go
+++ main.go
@@ -1,11 +1,4 @@
 package main
 
-import (
-	"fmt"
-	"strings"
-)
-
 func main() {
-	a := "a"
-	fmt.Println(len(strings.Split(a, ",")))
 }
--- pkg/match_bracket/match_bracket.go
+++ pkg/match_bracket/match_bracket.go
@@ -1,51 +1,80 @@
 package match_bracket
 
-import (
-	set "github.com/deckarep/golang-set"
-	arraystack "github.com/practice/pkg/stack/array"
-	"strings"
-	"text/scanner"
-)
-
-var rightBracket = set.NewSet(")", "]", "}")
-var leftBracket = set.NewSet("(", "[", "{")
 var bracketRelation = map[string]string{"(": ")", "[": "]", "{": "}"}
+var rightBracket = map[string]struct{}{
+	")": {},
+	"]": {},
+	"}": {},
+}
+var leftBracket = map[string]struct{}{
+	"(": {},
+	"[": {},
+	"{": {},
+}
+
+func MatchBracket1(s string) bool {
+    n := len(s)
+    if n % 2 == 1 {
+        return false
+    }
+    pairs := map[byte]byte{
+        ')': '(',
+        ']': '[',
+        '}': '{',
+    }
+    stack := []byte{}
+    for i := 0; i < n; i++ {
+        if pairs[s[i]] > 0 {
+            if len(stack) == 0 || stack[len(stack)-1] != pairs[s[i]] {
+                return false
+            }
+            stack = stack[:len(stack)-1]
+        } else {
+            stack = append(stack, s[i])
+        }
+    }
+    return len(stack) == 0
+}
 
 func MatchBracket(expression string) bool {
-	Brackets := arraystack.New()
-	sc := new(scanner.Scanner)
-	sc.Init(strings.NewReader(expression))
-	sc.Mode = scanner.ScanIdents | scanner.ScanInts | scanner.ScanFloats
-	var tok rune
-	for tok != scanner.EOF {
-		tok = sc.Scan()
-		e := sc.TokenText()
-
-		// skip empty string
-		if 0 == len(e) {
+	brackets := make([]string, 0)
+	for _, e := range expression {
+		s := string(e)
+		if !isBracket(s) {
 			continue
 		}
 
-		// push e into stack if e is a left bracket,
-		// check e and the top of stack if e is a right bracket:
-		// 1. if match(bracketRelation[stack.top] = e), scan the remainder of the string
-		// 2. not match, return false
-		if leftBracket.Contains(e) {
-			Brackets.Push(e)
-		} else if rightBracket.Contains(e) {
-			// no left bracket, return false
-			if Brackets.IsEmpty() {
+		// push e into stack if e is a left bracket
+		if isLeftBracket(s) {
+			brackets = append(brackets, s)
+		} else if isRightBracket(s) {
+			if len(brackets) == 0 {
 				return false
 			}
 
-			top := Brackets.Pop().(string)
-			if v := bracketRelation[top]; e != v {
+			// pop the top of stack if e is a right bracket
+			top := brackets[len(brackets)-1]
+			brackets = brackets[:len(brackets)-1]
+
+			if v := bracketRelation[top]; v != s {
 				return false
 			}
 		}
-
 	}
 
-	// if stack is empty, return true
-	return Brackets.IsEmpty()
+	return len(brackets) == 0
+}
+
+func isBracket(a string) bool {
+	return isLeftBracket(a) || isRightBracket(a)
+}
+
+func isLeftBracket(a string) bool {
+	_, ok := leftBracket[a]
+	return ok
+}
+
+func isRightBracket(a string) bool {
+	_, ok := rightBracket[a]
+	return ok
 }
